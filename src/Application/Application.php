78b99b496d40e9c2c506bf977cfdfabbeed9a5d2
Marek Bartos <bartos.developer152@gmail.com>
2019-04-14 00:36:10 +0200

    Application: Separate base code into BaseApplication

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -6,9 +6,8 @@ use Apitte\Core\Dispatcher\IDispatcher;
 use Apitte\Core\Http\ApiRequest;
 use Apitte\Core\Http\ApiResponse;
 use Contributte\Psr7\Psr7Response;
-use Contributte\Psr7\Psr7ServerRequestFactory;
 
-class Application implements IApplication
+class Application extends BaseApplication
 {
 
 	/** @var IDispatcher */
@@ -19,42 +18,9 @@ class Application implements IApplication
 		$this->dispatcher = $dispatcher;
 	}
 
-	public function run(): void
+	protected function dispatch(ApiRequest $request): ApiResponse
 	{
-		$request = new ApiRequest(Psr7ServerRequestFactory::fromGlobal());
-		$this->runWith($request);
-	}
-
-	public function runWith(ApiRequest $request): void
-	{
-		$response = new ApiResponse(new Psr7Response());
-
-		$response = $this->dispatcher->dispatch($request, $response);
-
-		$httpHeader = sprintf(
-			'HTTP/%s %s %s',
-			$response->getProtocolVersion(),
-			$response->getStatusCode(),
-			$response->getReasonPhrase()
-		);
-
-		header($httpHeader, true, $response->getStatusCode());
-
-		foreach ($response->getHeaders() as $name => $values) {
-			foreach ($values as $value) {
-				header(sprintf('%s: %s', $name, $value), false);
-			}
-		}
-
-		$stream = $response->getBody();
-
-		if ($stream->isSeekable()) {
-			$stream->rewind();
-		}
-
-		while (!$stream->eof()) {
-			echo $stream->read(1024 * 8);
-		}
+		return $this->dispatcher->dispatch($request, new ApiResponse(new Psr7Response()));
 	}
 
 }
---
+++ src/Application/BaseApplication.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types = 1);
+
+namespace Apitte\Core\Application;
+
+use Apitte\Core\Http\ApiRequest;
+use Apitte\Core\Http\ApiResponse;
+use Contributte\Psr7\Psr7ServerRequestFactory;
+
+abstract class BaseApplication implements IApplication
+{
+
+	public function run(): void
+	{
+		$request = new ApiRequest(Psr7ServerRequestFactory::fromSuperGlobal());
+		$this->runWith($request);
+	}
+
+	public function runWith(ApiRequest $request): void
+	{
+		$response = $this->dispatch($request);
+		$this->sendResponse($response);
+	}
+
+	abstract protected function dispatch(ApiRequest $request): ApiResponse;
+
+	protected function sendResponse(ApiResponse $response): void
+	{
+		$httpHeader = sprintf(
+			'HTTP/%s %s %s',
+			$response->getProtocolVersion(),
+			$response->getStatusCode(),
+			$response->getReasonPhrase()
+		);
+
+		header($httpHeader, true, $response->getStatusCode());
+
+		foreach ($response->getHeaders() as $name => $values) {
+			foreach ($values as $value) {
+				header(sprintf('%s: %s', $name, $value), false);
+			}
+		}
+
+		$stream = $response->getBody();
+
+		if ($stream->isSeekable()) {
+			$stream->rewind();
+		}
+
+		while (!$stream->eof()) {
+			echo $stream->read(1024 * 8);
+		}
+	}
+
+}
